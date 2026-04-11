b757d05e9f44539aedd97b2f0112098554966d1f
luminadesignerpro <luminadesigner@outlook.com.br>
2026-04-11 15:18:02 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/">
+     <BrowserRouter basename="/esterjorge15anos">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
